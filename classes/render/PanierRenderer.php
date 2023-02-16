7518f58fa0db198cc1c5350819e11694bcfb1625
Theo.pz <100997816+pellizza1u@users.noreply.github.com>
2023-02-16 16:08:19 +0100

    PanierRenderer

--- classes/render/PanierRenderer.php
+++ classes/render/PanierRenderer.php
@@ -2,19 +2,39 @@
 
 namespace ccd\render;
 
-class PanierRenderer
+class PanierRenderer implements Renderer
 {
-    public function render($panier): string
+    public function render(): string
     {
-        $html = '<table>';
-        foreach ($panier->getContenu() as $produit) {
-            $html .= '<tr>';
-            $html .= '<td>' . $produit['produit']->getNom() . '</td>';
-            $html .= '<td>' . $produit['quantite'] . '</td>';
-            $html .= '<td>' . $produit['produit']->getPrix() . '</td>';
-            $html .= '</tr>';
-        }
-        $html .= '</table>';
-        return $html;
+        return <<<HTML
+        <div class="panier">
+            <h2>Votre panier</h2>
+            <div class="produits">
+                <div class="produit">
+                    <div class="image">
+                        <img src="" alt="Produit 1">
+                    </div>
+                    <div class="description">
+                        <h2>Produit 1</h2>
+                        <p>Produit 1</p>
+                        <p>10 €</p>
+                    </div>
+                </div>
+                <div class="produit">
+                    <div class="image">
+                        <img src="" alt="Produit 2">
+                    </div>
+                    <div class="description">
+                        <h2>Produit 2</h2>
+                        <p>Produit 2</p>
+                        <p>20 €</p>
+                    </div>
+                </div>
+            </div>
+            <div class="total">
+                <p>Total : 30 €</p>
+            </div>
+        </div>
+        HTML;
     }
 }
